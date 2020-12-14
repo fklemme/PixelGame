a1790b8758b3dfb2c61a9c7ca9242dfc9b69021f
Florian Klemme <mail@florianklemme.de>
2020-12-14 20:17:38 +0100

    Played around.

--- source/main.cpp
+++ source/main.cpp
@@ -1,31 +1,41 @@
+#include <filesystem>
+#include <memory>
+
 #include "olcPixelGameEngine.hpp"
 
 // Override base class with your custom functionality
 class PixelGame : public olc::PixelGameEngine {
   public:
-    PixelGame() { 
-        // TODO: Show credit to OLC in the game (as part of license)
-        sAppName = "PixelGame";
-    }
+    PixelGame() { sAppName = "PixelGame"; }
 
   public:
     bool OnUserCreate() override {
         // Called once at the start, so create things here
+        if (!std::filesystem::is_regular_file("resources/olc_pge2_logo.png")) return false;
+        olc_pge2_logo = std::make_unique<olc::Sprite>("resources/olc_pge2_logo.png");
+
         return true;
     }
 
     bool OnUserUpdate(float fElapsedTime) override {
-        // Called once per frame, draws random coloured pixels
-        for (int x = 0; x < ScreenWidth(); x++)
-            for (int y = 0; y < ScreenHeight(); y++)
-                Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));
+        auto border = 5;
+
+        auto logo_xoffset = ScreenWidth() - olc_pge2_logo->width - border;
+        auto logo_yoffset = ScreenHeight() - olc_pge2_logo->height - border;
+        DrawSprite(logo_xoffset, logo_yoffset, olc_pge2_logo.get());
+
+        DrawString(logo_xoffset - 8 * 11, ScreenHeight() - (olc_pge2_logo->height + 8) / 2 - border, "Powered by");
+
         return true;
     }
+
+  private:
+    std::unique_ptr<olc::Sprite> olc_pge2_logo;
 };
 
 int main() {
     PixelGame game;
-    game.Construct(192, 108, 4, 4);
+    game.Construct(640, 360, 2, 2);
     game.Start();
     return EXIT_SUCCESS;
 }
